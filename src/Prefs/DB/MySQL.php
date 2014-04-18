ba8bc63bfc2a03e28c26869b4e05f1e43add305c
demetrio rey <demetrio.rey@gmail.com>
2014-04-18 11:36:27 -0430

    moving and fixing

--- src/Prefs/DB/MySQL.php
+++ src/Prefs/DB/MySQL.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Prefs\Storage;
+namespace Prefs\DB;
 use PrefsBridge\FwBridge;
 
 
--- src/Prefs/Prefs.php
+++ src/Prefs/Prefs.php
@@ -2,16 +2,16 @@
 
 use PrefsBridge\FwBridge;
 
-
 class Prefs {
-	public function __construct($storage) {
-		$this->storage = $storage;
+	public db;
+	public function __construct($db) {
+		$this->db = $db;
 	}
 	function get($name, $default='', $username=NULL ) {
 		if (!isset($username)) {
 			$username = FwBridge::userid();
 		}
-		$row = $this->storage->get($name, $username);
+		$row = $this->db->get($name, $username);
 		if ($row===false) {
 			return $default;
 		}
@@ -21,26 +21,26 @@ class Prefs {
 		if (!isset($username)) {
 			$username = FwBridge::userid();
 		}
-		$this->storage->set($name, $value, $username);
+		$this->db->set($name, $value, $username);
 	}
 	function getAll($username=NULL) {
 		if (!isset($username)) {
 			$username = FwBridge::userid();
 		}
-		$all = $this->storage->getAll($username);
+		$all = $this->db->getAll($username);
 		return $all;
 	}
 	function delete($name, $username=NULL) {
 		if (!isset($username)) {
 			$username = FwBridge::userid();
 		}
-		$this->storage->delete($name, $username);
+		$this->db->delete($name, $username);
 	}
 	function deleteAll($username=NULL) {
 		if (!isset($username)) {
 			$username = FwBridge::userid();
 		}
-		$this->storage->deleteAll($username);
+		$this->db->deleteAll($username);
 	}
 }
 
